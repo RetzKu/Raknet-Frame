a93418d190958d0eeb9af657fd8dc2c7a5677b40
Mika Kaunisto <mikakaunisto@kamk.fi>
2018-01-18 01:01:35 +0200

    Init commit

---
+++ Raknet-Client/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <iostream>
+#include <WinSock2.h>
+#include <Windows.h>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <conio.h>
+#include <thread>
+
+#include <Raknet\RakPeerInterface.h>
+#include <Raknet\MessageIdentifiers.h>
+#include <Raknet\RakNetTypes.h>
+#include <Raknet\BitStream.h>
+
+int main()
+{
+	return 0;
+}
